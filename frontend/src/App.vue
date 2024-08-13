38ee9beec2ed91b348e7b7f98229d7c954b217f6
hezefeng <hzf1998@126.com>
2024-08-13 10:42:11 +0800

    update

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,88 @@
 <template>
   <div id="app">
-    <router-view/>
+    <el-container>
+      <el-aside width="600px" v-if="!$route.meta.noMenu">
+        <el-row class="tac">
+          <el-col :span="12">
+            <h5>菜单栏</h5>
+            <el-menu
+                default-active="2"
+                class="el-menu-vertical-demo">
+              <el-menu-item v-for="menuItem in this.menuList" :key="menuList.ID" @click="goPages(menuItem.Path)">
+                <i class="el-icon-s-claim"></i>
+                <span slot="title">{{ menuItem.Name}}</span>
+              </el-menu-item>
+            </el-menu>
+          </el-col>
+        </el-row>
+      </el-aside>
+      <el-container>
+        <el-main>
+          <!--  用于右侧显示内容区          -->
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
+
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      menuList: []
+    }
+  },
+  methods: {
+    goAbout() {
+      this.$router.push("/about").catch(() => {
+      })
+    },
+    goViewArticles() {
+      this.$router.push("/viewArticles").catch(() => {
+      })
+    },
+    goPages(url){
+      console.log(url)
+      this.$router.push(url).catch(()=>{})
+    },
+    //获取当前用户的菜单列表
+    getMenu() {
+      const token = this.getToken()
+      if (token) {
+        axios.get("http://127.0.0.1:8088/v2/sys/menu", {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        }).then(res => {
+          this.menuList = JSON.parse(JSON.stringify(res.data.data))
+        })
+      }
+    },
+    // 获取用户的token
+    getToken() {
+      let cookieValue = null;
+      document.cookie.split('; ').forEach((item) => {
+        let [n, v] = item.trim().split('=');
+        if (n === 'jwt') {
+          cookieValue = v;
+        }
+      });
+      return cookieValue;
+    }
+  },
+  mounted() {
+    this.getMenu()
+  }
+}
+
+
+</script>
+
 
 <style>
 #app {
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import About from '../views/AboutView.vue'
+import ViewArticles from '../views/ViewArticle.vue'
+import WriteArticles from '../views/WriteArticle.vue'
+import Login from '../views/Login.vue'
+import Register from '../views/SignIn.vue'
 
 Vue.use(VueRouter)
 
@@ -8,9 +13,37 @@ const routes = [
     {
         path: '/',
         name: 'home',
-        component: HomeView
+        component: HomeView,
+        meta: {noMenu: false},
     },
-    {}
+    {
+        path: '/about',
+        name: 'about',
+        component: About,
+        meta: {noMenu: false},
+    }, {
+        path: '/viewArticles',
+        name: 'viewArticles',
+        component: ViewArticles,
+        meta: {noMenu: false},
+    }, {
+        path: '/writeArticles',
+        name: 'writeArticles',
+        component: WriteArticles,
+        meta: {noMenu: false},
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: Login,
+        meta: {noMenu: true},
+    }, {
+        path: '/signin',
+        name: 'signin',
+        component: Register,
+        meta: {noMenu: true}
+
+    }
 ]
 
 const router = new VueRouter({
