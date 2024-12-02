979680a3ced8e1376e392503c5c4f055c69dc532
hezefeng <hzf1998@126.com>
2024-12-02 21:13:02 +0800

    update

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,5 +1,67 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>个人主页</h1>
+    <div class="user-info">
+        <div class="avatar">
+          <el-image class="avatar-image" :src="avatarUrl"></el-image>
+        </div>
+        <div class="user-info-group">
+          <div class="user-info-name">用户名</div>
+          <div class="user-info-about">关于我</div>
+        </div>
+    </div>
   </div>
 </template>
+
+
+<script>
+export default {
+  name: 'AboutView',
+  data(){
+    return {
+      avatarUrl :  require('@/assets/avatar.png')
+    }
+  }
+}
+
+</script>
+
+<style>
+.user-info{
+  margin-left: 10px;
+  height: 200px;
+  width: 500px;
+  background-color: #2c3e50;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.avatar{
+  flex:1;
+}
+
+.user-info-group {
+  flex: 3;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 0 10px;
+}
+
+.user-info-name {
+  flex: 1;
+  background-color: #e9faf2;
+}
+
+.user-info-about {
+  flex: 1;
+  background-color: bisque;
+}
+
+.avatar-image{
+  width: 100px;
+  height: 100px;
+}
+
+</style>
