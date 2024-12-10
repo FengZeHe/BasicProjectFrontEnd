be4657e9da42099d191aa294d1289f25562a4f3e
hezefeng <hzf1998@126.com>
2024-12-10 20:26:32 +0800

    update

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -3,7 +3,8 @@
     <h1>个人主页</h1>
     <div class="user-info">
         <div class="avatar">
-          <el-image class="avatar-image" :src="avatarUrl"></el-image>
+<!--          <el-image class="avatar-image" :src="avatarUrl"></el-image>-->
+          <img :src="imageSrc" :key="imageSrc" alt="未获取图片">
         </div>
         <div class="user-info-group">
           <div class="user-info-name">用户名</div>
@@ -15,17 +16,55 @@
 
 
 <script>
+import axios from '@/axios';
+import { ref,onMounted } from 'vue';
+
 export default {
   name: 'AboutView',
-  data(){
+  data() {
     return {
-      avatarUrl :  require('@/assets/avatar.png')
+      avatarUrl: require('@/assets/avatar.png'),
+      imageSrc: '',
+      base64Data: '' // 用于存储从后端接收的base64数据
+    };
+  },
+  mounted() {
+    this.getBase64Image();
+  },
+  methods: {
+    getBase64Image() {
+      // 准备formData对象
+      const formData = new FormData();
+      formData.append('fileName', 'MyPhoto.png');
+
+      axios.post("/users/downloadFile", formData)
+          .then((res) => {
+            console.log(res);
+            this.base64Data = res.data.pic;
+            this.imageSrc = 'data:image/png;base64,' + this.base64Data;
+            this.convertBase64ToImage();
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+    },
+    convertBase64ToImage() {
+      const img = new Image();
+      img.src = this.imageSrc;
+      console.log(img.src);
+      img.onload = () => {
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        context.drawImage(img, 0, 0);
+        const imageURL = canvas.toDataURL('image/png');
+        this.imageSrc = imageURL; // 更新imageSrc
+      };
     }
   }
-}
-
+};
 </script>
-
 <style>
 .user-info{
   margin-left: 10px;
